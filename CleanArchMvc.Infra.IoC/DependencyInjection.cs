4e4e5dd8a1833f5217676d9167214d13e2b4e7d3
Silvio Queiroz <silviosouza1207@gmail.com>
2025-03-07 12:07:12 -0300

    Implementation Views Create, Delete, Details Category and Product

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,15 +24,30 @@ namespace CleanArchMvc.Infra.IoC
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
+            services.AddIdentity<ApplicationUser, IdentityRole>() // Inclui a configuração do identity padrão para usuários e roles
+                .AddEntityFrameworkStores<ApplicationDbContext>()// Inclui a implementação do EF Core do Identity para realizar a persistencia no SQL Server
+                .AddDefaultTokenProviders(); // Adiciona provedores de token para gerar tokens de redefinição de senha, alteração de email e etc
+
+            //Sempre que o usuário acessar uma rota que ele não possui acesso, irá redirecionar para "account/login"
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Account/Login");
+
             return services;
         }
 
         public static IServiceCollection AddServices(this IServiceCollection services)
         {
+            //Mediator
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+            //Services
             services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<ICategoryService, CategoryService>();            
-            services.AddAutoMapper(typeof(DomainToDtoMappingProfile));            
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+            //AutoMappers
+            services.AddAutoMapper(typeof(DomainToDtoMappingProfile));
+            services.AddAutoMapper(typeof(DTOToCommandMappingProfile));
 
             return services;
         }
--- CleanArchMvc.WebUI/Controllers/ProductController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductController.cs
@@ -1,19 +1,26 @@
-﻿using CleanArchMvc.Application.DTO;
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTO;
 using CleanArchMvc.Application.Interfaces;
-using CleanArchMvc.Application.UseCases.Products.Handlers;
+using CleanArchMvc.Application.UseCases.Products.Commands;
 using CleanArchMvc.Application.UseCases.Products.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IMediator _mediator;
-
-        public ProductController(IMediator mediator)
+        private readonly IMapper _mapper;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
+        public ProductController(IMediator mediator, IMapper mapper, ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _mediator = mediator;
+            _mapper = mapper;
+            _categoryService = categoryService;
+            _environment = environment;
         }
 
         [HttpGet]
@@ -21,6 +28,88 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             IEnumerable<ProductDTO> products = await _mediator.Send(new GetProductsQuery(), CancellationToken.None);
             return View(products);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategoriesAsync(CancellationToken.None), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductCreateCommand createCommand = _mapper.Map<ProductCreateCommand>(productDto);
+                await _mediator.Send(createCommand, CancellationToken.None);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            ProductDTO productDto = await _mediator.Send(new GetProductByIdQuery(id.GetValueOrDefault()), CancellationToken.None);
+
+            if (productDto == null) return NotFound();
+
+            var categories = await _categoryService.GetCategoriesAsync(CancellationToken.None);
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductUpdateCommand updateCommand = _mapper.Map<ProductUpdateCommand>(productDto);
+                await _mediator.Send(updateCommand, CancellationToken.None);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            ProductDTO productDto = await _mediator.Send(new GetProductByIdQuery(id.GetValueOrDefault()), CancellationToken.None);
+
+            if (productDto == null)
+                return NotFound();
+
+            return View(productDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            ProductRemoveCommand removeCommand = new ProductRemoveCommand(id);
+            await _mediator.Send(removeCommand, CancellationToken.None);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            ProductDTO productDto = await _mediator.Send(new GetProductByIdQuery(id.GetValueOrDefault()), CancellationToken.None);
+
+            if (productDto == null) return NotFound();
+            var wwwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + productDto.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+
+            return View(productDto);
         }
     }
 }
